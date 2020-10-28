e932e976bcdb4f83c2861ed7afb7d8ad5c7657cf
Rajat Lohia <rajatlohia1108@gmail.com>
2020-10-28 14:45:13 +0530

    installed ejs for views

--- WebServer/routes/index.js
+++ WebServer/routes/index.js
@@ -9,14 +9,3 @@ router.get('/mask', homeController.mask);
 router.get('/counter', homeController.counter);
 
 module.exports = router;
-
-app.get('/', (req, res) => {
-    var pyProcess = cmd.get('',
-    function(data, err, stderr){
-    if (!err)
-        res.send(data);
-    else 
-        console.log("python script cmd error: " + err)
-    }
-);
-});
