a4f208c35fe955d2efe0d980d80a749cc18faffc
Rajat Lohia <rajatlohia1108@gmail.com>
2020-10-28 14:27:24 +0530

    replaced child process with node-cmd

--- index.js
+++ index.js
@@ -1,19 +1,17 @@
 const express = require('express')
-const {spawn} = require('child_process');
 const app = express()
+var cmd = require('node-cmd')
 const port = 8000
 app.get('/', (req, res) => {
- 
- var dataToSend;
- const python = spawn('python', ['check.py']);
- python.stdout.on('data', function (data) {
-  console.log('Pipe data from python script ...');
-  dataToSend = data.toString();
- });
- python.on('close', (code) => {
- console.log(`child process close all stdio with code ${code}`);
- res.send(dataToSend)
- });
-})
+    var pyProcess = cmd.get('',
+    function(data, err, stderr){
+    if (!err)
+        res.send(data);
+    else 
+        console.log("python script cmd error: " + err)
+    }
+);
+});
+
 app.listen(port, () => console.log(`Example app listening on port 
 ${port}!`))
